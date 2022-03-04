b42bb1387a705019f6172e3898b83c6bce72f515
Didier <did35@yahoo.com>
2022-03-04 11:26:16 +0100

    Initial Commit

---
+++ ViewIndex/ViewIndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewIndexApp.swift
+//  ViewIndex
+//
+//  Created by Didier Delhaisse on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewIndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
